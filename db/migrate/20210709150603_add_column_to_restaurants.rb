c548a3483fdfbe6fc3425856a606379e70dcf01a
honoka2001 <honoka519pk@gmail.com>
2021-07-10 01:03:14 +0900

    add : Restaurantモデルにカラムを追加

---
+++ db/migrate/20210709150603_add_column_to_restaurants.rb
@@ -0,0 +1,6 @@
+class AddColumnToRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    add_column :restaurants, :name, :string, null: false
+    add_column :restaurants, :address, :string, null: false
+  end
+end
