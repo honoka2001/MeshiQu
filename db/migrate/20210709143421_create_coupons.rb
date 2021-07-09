39a4030795f8520cfaddee54d2b75f892c5e575a
honoka2001 <honoka519pk@gmail.com>
2021-07-10 01:01:32 +0900

    add : Couponモデル作成

---
+++ db/migrate/20210709143421_create_coupons.rb
@@ -0,0 +1,12 @@
+class CreateCoupons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :coupons do |t|
+      t.integer :min_setting_count, null: false
+      t.text :content, null: false
+      t.date :exp_date
+      t.references :restaurant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
