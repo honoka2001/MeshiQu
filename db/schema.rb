a482ace0fbfb8f36b92c788ce5561e781c3725f1
honoka2001 <honoka519pk@gmail.com>
2021-07-10 01:04:56 +0900

    add : DB作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_120701) do
+ActiveRecord::Schema.define(version: 2021_07_09_154928) do
+
+  create_table "coupons", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "min_setting_count", null: false
+    t.text "content", null: false
+    t.date "exp_date"
+    t.bigint "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_coupons_on_restaurant_id"
+  end
+
+  create_table "event_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "event_id"
+    t.bigint "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_users_on_event_id"
+    t.index ["member_id"], name: "index_event_users_on_member_id"
+  end
+
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "setting_count", null: false
+    t.string "prace", null: false
+    t.text "content", null: false
+    t.date "event_date", null: false
+    t.date "deadline_date"
+    t.bigint "coupon_id"
+    t.bigint "host_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["coupon_id"], name: "index_events_on_coupon_id"
+    t.index ["host_id"], name: "index_events_on_host_id"
+  end
 
   create_table "restaurants", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +53,8 @@ ActiveRecord::Schema.define(version: 2021_07_09_120701) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "address", null: false
     t.index ["email"], name: "index_restaurants_on_email", unique: true
     t.index ["reset_password_token"], name: "index_restaurants_on_reset_password_token", unique: true
   end
@@ -32,8 +67,18 @@ ActiveRecord::Schema.define(version: 2021_07_09_120701) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "school"
+    t.integer "grade"
+    t.string "department"
+    t.integer "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "coupons", "restaurants"
+  add_foreign_key "event_users", "events"
+  add_foreign_key "event_users", "users", column: "member_id"
+  add_foreign_key "events", "coupons"
+  add_foreign_key "events", "users", column: "host_id"
 end
