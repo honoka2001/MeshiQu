4994f032cdae9e393ee31a09f70ddd4b789fc358
honoka2001 <honoka519pk@gmail.com>
2021-07-10 22:09:26 +0900

    add : restaurantsコントローラー作成

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,10 @@
+class RestaurantsController < ApplicationController
+  before_action :authenticate_restaurant!, except: [:show, :index]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
