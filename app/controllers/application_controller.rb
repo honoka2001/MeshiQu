87d39fc99d89204f1444725d56e12546b54a0728
honoka2001 <honoka519pk@gmail.com>
2021-07-10 14:24:31 +0900

    change : UserとRestaurantごとにサニタイザーを定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,29 @@
+class User::ParameterSanitizer < Devise::ParameterSanitizer
+    def initialize(*)
+        super
+        permit(:sign_up, keys: [:name, :school, :grade, :department, :gender, :email, :password])
+    end
+end
+
+class Restaurant::ParameterSanitizer < Devise::ParameterSanitizer
+    def initialize(*)
+        super
+        permit(:sign_up, keys: [:name, :address, :email, :password])
+    end
+end
+
+
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
     protected
      
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :school, :grade, :department, :gender])
+    def devise_parameter_sanitizer
+        if resource_class == User
+          User::ParameterSanitizer.new(User, :user, params)
+        elsif resource_class == Restaurant
+          Restaurant::ParameterSanitizer.new(Restaurant, :restaurant, params)
+        else
+          super # Use the default one
+        end
     end
 end
