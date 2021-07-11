1d85dc8881e002298714c703228fe66a2f626477
honoka2001 <honoka519pk@gmail.com>
2021-07-11 23:26:39 +0900

    add : Eventsコントローラーのcreateアクション作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,4 +9,19 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    @event = Event.new(event_params)
+    @event.host_id = current_user.id
+    if @event.save!
+        redirect_to event_path(@event), notice: '投稿に成功しました。'
+      else
+        render :new
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:setting_count, :prace, :content, :event_date, :deadline_date)
+  end
+
 end
