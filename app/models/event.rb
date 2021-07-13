584fca4718f73e26b904599669783f620fba9da9
honoka2001 <honoka519pk@gmail.com>
2021-07-13 12:33:06 +0900

    fix : EventのUserに紐づく外部キーをhost_idに指定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-    belongs_to :user, optional: true
+    belongs_to :host, class_name: 'User', foreign_key: :host_id
     belongs_to :coupon
     has_many :event_users
     has_many :users, through: :event_users
