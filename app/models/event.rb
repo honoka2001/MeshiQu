af75e22a795de49b36e55f22dff8566ab5d0de64
honoka2001 <honoka519pk@gmail.com>
2021-07-13 12:50:08 +0900

    fix : paticipate_memberに名前を修正しuserモデルに紐づける

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
     belongs_to :host, class_name: 'User', foreign_key: :host_id
     belongs_to :coupon
+
     has_many :event_users
-    has_many :users, through: :event_users
+    has_many :participate_members , through: :event_users, source: :user
 end
