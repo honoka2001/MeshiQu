301ab97a1628601673f457ddad617812e5cafc80
Yoshito-4410 <ttymah@icloud.com>
2021-07-13 16:55:31 +0900

    コメントの修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -213,7 +213,7 @@ Devise.setup do |config|
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   # config.unlock_in = 1.hour
 
-  #  on the last attempt before the account is locked.
+  # Warn on the last attempt before the account is locked.
   # config.last_attempt_warning = true
 
   # ==> Configuration for :recoverable
