9f7464aa742ae9217fae9594cf1c736d6b5edd98
honoka2001 <honoka519pk@gmail.com>
2021-07-14 23:53:21 +0900

    fix : 中間テーブルを介したリレーションのテーブルの別名変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
 
   has_many :events, dependent: :destroy, foreign_key: :host_id
   has_many :event_users, foreign_key: :member_id
-  has_many :participate_events, through: :event_users, source: :event
+  has_many :participates, through: :event_users, source: :event
+
+
 end
