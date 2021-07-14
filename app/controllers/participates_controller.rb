c61dad23e3591f4af762f4d79d76c8ae22ec2b5d
honoka2001 <honoka519pk@gmail.com>
2021-07-14 23:57:28 +0900

    add : participatesコントローラーにユーザーがイベントの参加解除するアクション作成

--- app/controllers/participates_controller.rb
+++ app/controllers/participates_controller.rb
@@ -3,4 +3,11 @@ class ParticipatesController < ApplicationController
         @participate = EventUser.create(event_id: params[:event_id], member_id: current_user.id)
         redirect_back(fallback_location: root_path)
     end
+
+    def destroy
+        @event = Event.find(params[:event_id])
+        @participate = EventUser.find_by(event_id: @event.id, member_id: current_user)
+        @participate.destroy
+        redirect_back(fallback_location: root_path)
+    end
 end
