2e61db4b7e568806c5e177dcdb2f3b4bf94c5d90
honoka2001 <honoka519pk@gmail.com>
2021-07-17 04:13:24 +0900

    add : checksコントローラーのdestroyアクション作成

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -3,4 +3,10 @@ class ChecksController < ApplicationController
 		@check = current_user.checks.create(coupon_id: params[:coupon_id])
 		redirect_back(fallback_location: root_path)
 	end
+
+	def destroy
+		@check = Check.find_by(coupon_id: params[:coupon_id], user_id: current_user.id)
+		@check.destroy
+		redirect_back(fallback_location: root_path)
+	end
 end
