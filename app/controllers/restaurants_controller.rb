0450f7f95811146676ce44d5746da4e32ee7200f
Yoshito-4410 <ttymah@icloud.com>
2021-07-20 14:18:43 +0900

    add : restaurant の update action を作成

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,4 +11,19 @@ class RestaurantsController < ApplicationController
   def edit
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant), notice: '更新に成功しました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
 end
